f6d8badfc2d24d9f657080a238d2e54fb3216237
youssefbac <49171662+youssefbac@users.noreply.github.com>
2019-04-08 12:15:47 +0200

    Create deplaçersouris.h

---
+++ deplaçersouris.h
@@ -0,0 +1,7 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+#include <SDL/SDL.h>
+
+void deplacement(SDL_Surface *ecran,SDL_Surface *image ,int continuer,SDL_Rect position);
+
+#endif // JEU_H_INCLUDED
